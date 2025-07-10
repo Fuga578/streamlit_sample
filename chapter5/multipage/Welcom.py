ba0ef336bd1fcd79ec4bf209dd1217de04ab67d2
Fuga578 <kochikaze@softbank.ne.jp>
2025-07-10 19:14:04 +0900

    マルチページ処理実装

---
+++ chapter5/multipage/Welcom.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.header(':blue[枉駕来臨]')
+st.image('https://upload.wikimedia.org/wikipedia/commons/6/60/WELCOME.jpg',
+         width=400)
---
+++ chapter5/multipage/pages/1_💻_LaTeX.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.header("LaTex")
+
+st.latex(r"E^2 = (mc^2)^2 + (pc)^2")
